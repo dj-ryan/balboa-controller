30025888252d9b8e9655c575bf845edc28fe2d97
R.D. Wagner <callisto076@gmail.com>
2022-02-24 22:13:06 -0600

    Updated initial node to match checkpoint submission

--- src/proc_data.py
+++ src/proc_data.py
@@ -4,26 +4,45 @@ from std_msgs.msg import UInt8
 from geometry_msgs.msg import Twist
 from balboa_core.msg import balboaMotorSpeeds
 
-#from beginner_tutorials.srv import AddTwoInts,AddTwoIntsResponse
 import rospy
 
 pubMotor = rospy.Publisher('/motorSpeeds', balboaMotorSpeeds, queue_size = 10)
+motor = balboaMotorSpeeds()
 
 def callback(data):
     rospy.loginfo("data x: %f, data z: %f", data.linear.x, data.linear.z)
 
-    motorSpeed = balboaMotorSpeeds()
-    motorSpeed.left = 100
-    motorSpeed.right = 100
-    pubMotor.publish(motorSpeed)
+    if (data.linear.x < 0):
+        motor.left = 10
+        motor.right = 10
+    elif (data.linear.x > 0):
+        motor.left = -10
+        motor.right = -10
+    elif(data.angular.z < 0):
+        motor.left = 10
+        motor.right = -10
+    elif(data.angular.z > 0):
+        motor.left = -10
+        motor.right = 10
+    else:
+        motor.left = 0
+        motor.right = 0
 
 def teleop_listener():
     rospy.init_node('teleop_listener')
     rospy.loginfo("STARTED")
     rospy.Subscriber('/turtle1/cmd_vel', Twist, callback)
-    
+    rate = rospy.Rate(10)
+
     while not rospy.is_shutdown():
-        rospy.spin()
+        rospy.loginfo("in WHILE: %f", motor.left)
+        pubMotor.publish(motor)
+        motor.left = 0
+        motor.right = 0
+        rate.sleep()
 
 if __name__ == "__main__":
     teleop_listener()
+
+
+
