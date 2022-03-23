c8fb2001a4c78ac2c500332e41ebb4f864cb157c
KateyK <krullbethany@gmail.com>
2022-03-23 00:09:56 -0500

    commented IR Reactive Control

--- src/ir_reactive_control.cpp
+++ src/ir_reactive_control.cpp
@@ -10,60 +10,34 @@ float range = 0;
 
 void callback(const balboa_core::balboaLL &data)
 {
-    ROS_INFO("sensor: %d", data.rangeSensor);
-
+	//grab range sensor data from balboa robot
     range = data.rangeSensor;
 }
 
 int main(int argc, char **argv)
 {
-
+	//initiallize node/subscriber/publisher
     ROS_INFO("balba_controller/ir_reactiveControl node starting......");
-
     ros::init(argc, argv, "ir_reactive_control");
-
     ros::NodeHandle n;
-
     ros::Subscriber sub = n.subscribe("balboaLL", 1000, callback);
-
     ros::Publisher pub = n.advertise<geometry_msgs::Twist>("ir_reactive_control/cmd_vel", 1000);
-
     ros::Rate loop_rate(10);
-
-    // ros::spin();
 
     while (ros::ok())
     {
         geometry_msgs::Twist move;
 
+		//set target distance and calculate the distance robot needs to move
         int target = 300;
-
         int moveValue = target - range;
-
-        ROS_INFO("move: %d", moveValue);
-
         move.linear.x = moveValue;
 
+		//publish needed movement to topic for pidLab2.py to use
         pub.publish(move);
 
-        // if (range > 200) // go backwards
-        // {
-        //     move.linear.x = range;
-        //     pub.publish(move);
-        // }
-        // else if (range > 300) // go forwards
-        // {
-        //     move.linear.x = -range;
-        //     pub.publish(move);
-        // } else {
-        //     move.linear.x = 0;
-        //     pub.publish(move);
-        // }
-
         ros::spinOnce();
-        // delay
         loop_rate.sleep();
-
         ++loopCount;
     }
 
